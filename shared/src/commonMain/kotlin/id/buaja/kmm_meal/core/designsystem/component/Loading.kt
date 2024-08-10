bde5ef576a521481acf90308efdf274ecb0a2023
Egi10 <egifcb@gmail.com>
2024-08-10 07:16:47 +0700

    feat(lottie): change loading to lottie

--- shared/src/commonMain/kotlin/id/buaja/kmm_meal/core/designsystem/component/Loading.kt
+++ shared/src/commonMain/kotlin/id/buaja/kmm_meal/core/designsystem/component/Loading.kt
@@ -1,29 +1,41 @@
 package id.buaja.kmm_meal.core.designsystem.component
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.*
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import id.buaja.kmm_meal.resources.Res
+import io.github.alexzhirkevich.compottie.Compottie
+import io.github.alexzhirkevich.compottie.LottieCompositionSpec
+import io.github.alexzhirkevich.compottie.rememberLottieComposition
+import io.github.alexzhirkevich.compottie.rememberLottiePainter
+import org.jetbrains.compose.resources.ExperimentalResourceApi
 
+@OptIn(ExperimentalResourceApi::class)
 @Composable
 fun MealLoading(
     modifier: Modifier = Modifier
 ) {
+    val composition by rememberLottieComposition {
+        LottieCompositionSpec.JsonString(
+            Res.readBytes("files/loading-food.json").decodeToString()
+        )
+    }
+
     Box(
         contentAlignment = Alignment.Center,
         modifier = modifier
             .fillMaxSize()
     ) {
-        Column(
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
-        ) {
-            CircularProgressIndicator(
-                color = MaterialTheme.colors.primary,
-                strokeWidth = 4.dp,
-                modifier = Modifier.size(32.dp)
-            )
-        }
+        Image(
+            painter = rememberLottiePainter(
+                composition = composition,
+                iterations = Compottie.IterateForever
+            ),
+            contentDescription = "Lottie animation"
+        )
     }
 }
